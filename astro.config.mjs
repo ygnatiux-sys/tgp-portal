5830f27122c4285ba40c96a7e16c6fcb0ebcb6a1
Xavier Benitez • The Great Puzzle Project <ygnatiux@gmail.com>
2026-08-24 13:04:32 -0300

    fix: optimizacion de clases aspect-video y linter limpio

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,7 @@ export default defineConfig({
   
   image: {
     domains: [
+      'storage.thegreatpuzzleproject.com',
       'images.unsplash.com',
       'upload.wikimedia.org',
       'commons.wikimedia.org',
